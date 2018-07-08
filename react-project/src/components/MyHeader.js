5afd5a6d5f0635713a3a442fd1b76b31464341d9
dezhizhang <1541609448@qq.com>
2018-07-08 20:04:29 +0800

    登录界面功能的实现

--- react-project/src/components/MyHeader.js
+++ react-project/src/components/MyHeader.js
@@ -1,17 +1,73 @@
 import React from 'react';
-import {Layout,Row,Col,Menu,Icon} from 'antd';
+import {
+    Layout,
+    Row,
+    Col,
+    Menu,
+    Icon,
+    Tabs,
+    message,
+    Form,
+    Input,
+    Modal,
+    Button,
+    Checkbox
+} from 'antd';
 import { Link } from 'react-router-dom';
 import MenuItem from '../../node_modules/antd/lib/menu/MenuItem';
+import FormItem from '../../node_modules/antd/lib/form/FormItem';
 const {Header,Footer}=Layout;
 
 class MyHeader extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            currentKey:'soyea'
+            currentKey:'soyea',
+            visible:false,
+            action:'login',
+            userId:0,
+            userNickName:''
         }
     }
+    handleOk=()=>{
+        this.setState({
+            visible:false
+        })
+    }
+    fnSubmit=(ev)=>{
+
+       ev.preventDefault();
+    }
+    handleCancle=()=>{
+        this.setState({
+            visible:false
+        })
+    }
+    fnShowModal=(e)=>{
+       if(e.key='register'){
+           this.setState({currentKey:'register'});
+           this.setState({visible:true})
+       }else{
+            this.setState({currentKey:'e.key'});
+            this.setState({visible:true});
+       }
+
+    }
+    
     render(){
+        //let {getFileProps}=this.props.form;
+        let userShow=this.state.hasLogined ? 
+        <Menu.Item key='loginout'>
+            <Button type='primary'>{this.state.userNickName}</Button>
+            <Link target="_blank">
+              <Button type='dished' hemlType='button'>个人中心</Button>
+            </Link>
+            <Button type='ghost' htmlType='button'>退出登录</Button>
+        </Menu.Item>
+        :<Menu.Item key='register' onClick={this.fnShowModal}>
+            <Icon type='appstore'/>注册/登录
+        </Menu.Item>
+        
         return (<div>
            <Layout>
                <Header>
@@ -21,7 +77,7 @@ class MyHeader extends React.Component{
                          <a href='/' style={{fontSize:'20px',color:'#fff'}}>好利来有限公司</a>
                        </Col>
                        <Col span={16}>
-                         <Menu mode='horizontal' selectedKeys={[this.state.currentKey]} theme='dark' style={{marginTop:'10px'}}>
+                         <Menu mode='horizontal'  selectedKeys={[this.state.currentKey]} theme='dark' style={{marginTop:'10px'}}>
                              <MenuItem key='soyea'>
                                  <Icon type='desktop'/>首页
                              </MenuItem>
@@ -46,8 +102,34 @@ class MyHeader extends React.Component{
                              <MenuItem key='qingan'>
                                  <Icon type='user-add'/>情感
                              </MenuItem>
+                             {userShow}
                          </Menu>
                        </Col>
+                       <Modal
+                         title='用户中心'
+                         visible={this.state.visible}
+                         onOk={this.handleOk}
+                         onCancel={this.handleCancle}
+                         okText='关闭'
+                         cancelText='取消'
+                       >
+                           <Tabs type='card'>
+                               <Tabs.TabPane tab='注册' key={2}>
+                                   <Form horizontal onSubmit={this.fnSubmit}>
+                                     <FormItem label='帐户'>
+                                       <Input placeholder='请输入你的帐号'/>
+                                     </FormItem>
+                                     <FormItem label='密码'>
+                                       <Input type='password' placeholder='请输入你的密码' />
+                                     </FormItem>
+                                     <FormItem label='确认密码'>
+                                        <Input type='password' placeholder='请再次输入密码'/>
+                                     </FormItem>
+                                     <Button type='primary' htmlType='submit'>注册</Button>
+                                   </Form>
+                               </Tabs.TabPane>
+                           </Tabs>
+                       </Modal>
                        <Col span={2}></Col>
 
                    </Row>
